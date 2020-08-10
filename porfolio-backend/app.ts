e80f65eb2df45d476f38f39c6dd1372981f67853
Giang <nguyengiang.nchg@gmail.com>
2020-08-11 00:29:45 +0300

    add env file

--- porfolio-backend/app.ts
+++ porfolio-backend/app.ts
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/sendEmail', (req, res, next) => {
   const data = {
     from: req.body.name,
-    to : 'nguyengiang.nchg@gmail.com',
+    to : process.env.GMAIL,
     subject: req.body.name + req.body.title,
     text: req.body.content
   }
@@ -20,11 +20,10 @@ app.post('/sendEmail', (req, res, next) => {
     service: 'Gmail',
     auth: {
       type: "OAuth2",
-      user: 'nguyengiang.nchg@gmail.com',
-      clientId: '827944169415-vhvdmbp90c8cpk2emagbr1aftmads6kd.apps.googleusercontent.com',
-      clientSecret: 'i6d_rReAebbRyldcI44mSHFx',
-      refreshToken: '//04V146TAEKeLSCgYIARAAGAQSNwF-L9Ir372nr1XdfBFmZ56goXfGbPETjAte6igXoWZZFTG4E6S4hfA5ZO2I2PCLtcMCoLVBVz0',
-      accessToken: 'ya29.a0AfH6SMBpKn68pjyqGjaq9FPRV-kNaVOUZgPlDDwa5lApn8wLWN9e9OOpCKDbG8xT1_bTF8ysxP3Yk6K1oQAmmUTWRoauLAMHrk9kXEAQKe6ZCnT2u6HbVqe4T6T8WPi48NqQRldd6WA_55y-l5H2ob09HUU4h0O_wtU'
+      user: process.env.GMAIL,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENTSECRECT,
+      refreshToken: process.env.REFRESHTOKEN
     }
   })
 
